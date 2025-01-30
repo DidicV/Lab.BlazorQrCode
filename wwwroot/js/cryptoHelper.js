54a4fffccf55137a982ea938767a55f608c95428
Tobias Richling <richlint@apetito.com>
2025-01-30 10:15:24 +0100

    start wip

---
+++ wwwroot/js/cryptoHelper.js
@@ -0,0 +1,14 @@
+window.cryptoHelper = {
+
+    generateKeyPair: async function () {
+
+    },
+
+    signData: async function (privateKey, data) {
+
+    },
+
+    verifySignature: async function (publicKey, data, signature) {
+        
+    }
+}
